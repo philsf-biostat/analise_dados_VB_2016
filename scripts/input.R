eec69271744c15c98ba051a5f091c68df4fc82a4
Felipe Figueiredo <philsf79@gmail.com>
2016-04-20 10:39:11 -0300

    novas variaveis
    As variáveis de Dor (nos períodos imediato e tardio) foram discretizadas
    em categorias para simplificar a apresentação gráfica. Além disso, o
    código para criação dos níveis estava errado, e foi removido (comentado).

--- scripts/input.R
+++ scripts/input.R
@@ -2,9 +2,15 @@ dados <- read.table("dataset/vb-dataset.dat")
 
 dados$ASA <- ordered(dados$ASA)
 dados$Dor.Imediato <- ordered(dados$Dor.Imediato)
+dados$Dor.Imediato2 <- ordered(dados$Dor.Imediato2)
 dados$Dor.Tardio <- ordered(dados$Dor.Tardio)
+dados$Dor.Tardio2 <- ordered(dados$Dor.Tardio2)
 dados$Tempo.Cirurgia <- ordered(dados$Tempo.Cirurgia)
 dados$Tempo.Bloqueio <- ordered(dados$Tempo.Bloqueio)
+#levels(dados$Dor.Imediato) <- 0:10
+#levels(dados$Dor.Tardio) <- 0:10
+
+attach(dados)
 
 str(dados)
 summary(dados)
