41958209084d0680ca1b29a082ae43a562583efa
Felipe Figueiredo <philsf79@gmail.com>
2016-07-21 21:45:05 -0300

    figuras

--- scripts/cor.R
+++ scripts/cor.R
@@ -26,7 +26,7 @@ dev.off()
 par(mfrow = c(1,1))
 t <- table(Técnica, Tempo.Bloqueio); barplot(t, beside = T, legend.text = rownames(t), main = "Tempo de Bloqueio x técnica anestésica", xlab = "Tempo de Bloqueio", ylab = "Frequencia")
 
-par(mfrow = c(1,2))
+par(mfrow = c(2,2))
 
 t <- table(Técnica, Dor.Imediato); barplot(t, beside = T, legend.text = rownames(t), main = "Dor imediato x técnica anestésica", xlab = "Dor", ylab = "Frequencia")
 t <- table(Técnica, Dor.Imediato2); barplot(t, beside = T, legend.text = rownames(t), main = "Dor imediato x técnica anestésica", xlab = "Dor", ylab = "Frequencia")
@@ -47,11 +47,40 @@ boxplot(Dor.Imediato ~ Tempo.Cirurgia, data = B, xlab = "Tempo de cirurgia", yla
 boxplot(Dor.Imediato ~ Tempo.Bloqueio, data = B, xlab = "Tempo de bloqueio", ylab = "Dor tardio", main = "B")
 dev.off()
 
+png("figuras/boxplot-tempos_dor.png")
+par(mfrow = c(2,2))
+boxplot(Dor.Imediato2 ~ Tempo.Cirurgia, data = A, xlab = "Tempo de cirurgia", ylab = "Dor imediato", main = "A")
+boxplot(Dor.Imediato2 ~ Tempo.Cirurgia, data = B, xlab = "Tempo de cirurgia", ylab = "Dor imediato", main = "B")
+boxplot(Dor.Tardio2 ~ Tempo.Cirurgia, data = A, xlab = "Tempo de cirurgia", ylab = "Dor tardio", main = "A")
+boxplot(Dor.Tardio2 ~ Tempo.Cirurgia, data = B, xlab = "Tempo de cirurgia", ylab = "Dor tardio", main = "B")
+dev.off()
+
+## tempo x dor x grupo (26/4)
+png("figuras/barplot-dor-tempo-tecnica2.png")
+par(mfrow = c(2,2))
+t <- table(A$Tempo.Cirurgia, A$Dor.Imediato2); barplot(t, beside = T, legend.text = rownames(t), main = "A", xlab = "Imediato")
+t <- table(B$Tempo.Cirurgia, B$Dor.Imediato2); barplot(t, beside = T, legend.text = rownames(t), main = "B", xlab = "Imediato")
+t <- table(A$Tempo.Cirurgia, A$Dor.Tardio2); barplot(t, beside = T, legend.text = rownames(t), main = "A", xlab = "Tardio")
+t <- table(B$Tempo.Cirurgia, B$Dor.Tardio2); barplot(t, beside = T, legend.text = rownames(t), main = "B", xlab = "Tardio")
+dev.off()
+
 par(mfrow = c(1,1))
 
 par(mfrow = c(2,2))
 t <- table(Tempo.Cirurgia, Dor.Imediato); barplot(t, beside = T, legend.text = c("< 2h","> 2h"), main = "tempo de cirurgia", xlab = "Dor imediato", ylab = "Frequencia")
 t <- table(Tempo.Cirurgia, Dor.Tardio); barplot(t, beside = T, legend.text = c("< 2h","> 2h"), main = "tempo de cirurgia", xlab = "Dor tardio", ylab = "Frequencia")
+
+
+## Dor por técnica #### 26/04
+png("figuras/barplot-dor-tecnica.png")
+par(mfrow = c(2,1))
+t <- table(A$Técnica, A$Dor.Imediato2); barplot(t, beside = T, legend.text = rownames(t), main = "Dor imediato x técnica anestésica", xlab = "Dor", ylab = "Frequencia")
+t <- table(A$Técnica, A$Dor.Tardio2); barplot(t, beside = T, legend.text = rownames(t), main = "Dor tardio x técnica anestésica", xlab = "Dor", ylab = "Frequencia")
+t <- table(B$Técnica, B$Dor.Imediato2); barplot(t, beside = T, legend.text = rownames(t), main = "Dor imediato x técnica anestésica", xlab = "Dor", ylab = "Frequencia")
+t <- table(B$Técnica, B$Dor.Tardio2); barplot(t, beside = T, legend.text = rownames(t), main = "Dor tardio x técnica anestésica", xlab = "Dor", ylab = "Frequencia")
+dev.off()
+
+
 
 
 ## teste com variável ordinal ####
