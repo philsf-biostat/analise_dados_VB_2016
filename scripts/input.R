c648877dc811500e4c8237862873f45b61b9d2e2
Felipe Figueiredo <philsf79@gmail.com>
2016-07-21 21:23:32 -0300

    Dados re-padronizados. Closes #2. Closes #5.
    Fecha a issue #2, como finalização da etapa de padronização dos dados.
    
    Fecha a issue #5 pois a estrutura dos dados foi redefinida. Agora, a
    estratificação ficou como categorias binárias, portanto a ordem
    alfabética passou a ser aceitável.

--- scripts/input.R
+++ scripts/input.R
@@ -3,8 +3,10 @@ dados <- read.table("dataset/vb-dataset.dat")
 dados$ASA <- ordered(dados$ASA)
 dados$Dor.Imediato <- ordered(dados$Dor.Imediato)
 dados$Dor.Imediato2 <- ordered(dados$Dor.Imediato2)
+dados$Dor.Imediato3 <- ordered(dados$Dor.Imediato3)
 dados$Dor.Tardio <- ordered(dados$Dor.Tardio)
 dados$Dor.Tardio2 <- ordered(dados$Dor.Tardio2)
+dados$Dor.Tardio3 <- ordered(dados$Dor.Tardio3)
 dados$Tempo.Cirurgia <- ordered(dados$Tempo.Cirurgia)
 dados$Tempo.Bloqueio <- ordered(dados$Tempo.Bloqueio)
 #levels(dados$Dor.Imediato) <- 0:10
@@ -14,3 +16,5 @@ attach(dados)
 
 str(dados)
 summary(dados)
+
+detach(dados)
