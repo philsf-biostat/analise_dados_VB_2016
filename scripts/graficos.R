21c2ebdd3d823078ed57c67006ae8eabc9832e72
Felipe Figueiredo <philsf79@gmail.com>
2016-07-26 11:08:22 -0300

    caminho corrigido
    As figuras estavam sendo salvas na raiz do repositório. Corrigido.

--- scripts/graficos.R
+++ scripts/graficos.R
@@ -5,14 +5,14 @@ source("scripts/input.R")
 
 par(mfrow = c(1,1))
 t <- table(Grupo, Dor.Imediato3, Tempo.Cirurgia)[,,1]; barplot(t, beside = T, legend.text = rownames(t), main = paste("p-valor:",round(fisher.test(t)$p.value, 4)))
-dev.copy(png, "tempo-curto-dor-imediato.png"); dev.off()
+dev.copy(png, "figuras/tempo-curto-dor-imediato.png"); dev.off()
 t <- table(Grupo, Dor.Tardio3, Tempo.Cirurgia)[,,1]; barplot(t, beside = T, legend.text = rownames(t), main = paste("p-valor:",round(fisher.test(t)$p.value, 4)))
-dev.copy(png, "tempo-curto-dor-tardio.png"); dev.off()
+dev.copy(png, "figuras/tempo-curto-dor-tardio.png"); dev.off()
 
 t <- table(Grupo, Dor.Imediato3, Tempo.Cirurgia)[,,2]; barplot(t, beside = T, legend.text = rownames(t), main = paste("p-valor:",round(fisher.test(t)$p.value, 4)))
-dev.copy(png, "tempo-longo-dor-imediato.png"); dev.off()
+dev.copy(png, "figuras/tempo-longo-dor-imediato.png"); dev.off()
 t <- table(Grupo, Dor.Tardio3, Tempo.Cirurgia)[,,2]; barplot(t, beside = T, legend.text = rownames(t), main = paste("p-valor:",round(fisher.test(t)$p.value, 4)))
-dev.copy(png, "tempo-longo-dor-tardio.png"); dev.off()
+dev.copy(png, "figuras/tempo-longo-dor-tardio.png"); dev.off()
 
 rm(t)
 
@@ -21,4 +21,4 @@ source("scripts/cleanup.R")
 
 # obsolete ####
 # par(mfrow = c(2,2))
-# dev.copy(png, "tempo-dor-2x2.png"); dev.off()
+# dev.copy(png, "figuras/tempo-dor-2x2.png"); dev.off()
